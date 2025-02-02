d6d918185c376529febf9b890da733017ad90722
ibnu292605 <ibnuck08@gmail.com>
2025-02-02 16:14:44 +0700

    Menambahkan List

--- index.php
+++ index.php
@@ -8,5 +8,10 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos quod accusantium animi molestiae mollitia quam quidem soluta. Ipsa, temporibus. At quod provident officiis. Inventore, ab ipsum excepturi iste praesentium labore.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
+
 </body>
 </html>
